5fa5c3bd049c496975ed191e2295840152efb96a
karataymarufemre <karataymarufemre@gmail.com>
2024-10-28 23:10:13 +0000

    link executor updated

--- command-executor/src/main/java/dev/karatay/commandexecutor/core/executor/impl/LinkExecutor.java
+++ command-executor/src/main/java/dev/karatay/commandexecutor/core/executor/impl/LinkExecutor.java
@@ -1,25 +1,43 @@
 package dev.karatay.commandexecutor.core.executor.impl;
 
-import java.net.URI;
-
 import dev.karatay.commandexecutor.config.model.CommandElement;
 import dev.karatay.commandexecutor.core.executor.Executor;
+import dev.karatay.commandexecutor.domain.model.OSType;
 import dev.karatay.commandexecutor.error.CommandErrors;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class LinkExecutor implements Executor {
-	private CommandElement commandElement;
 
-	@Override
-	public void execute() {
-		commandElement.validate();
-		var desktop = java.awt.Desktop.getDesktop();
-		try {
-			URI oURL = new URI(commandElement.getValue());
-			desktop.browse(oURL);
-		} catch (Exception ex) {
-			throw CommandErrors.linkOpenError(commandElement, ex);
-		}
-	}
+    private CommandElement commandElement;
+
+    @Override
+    public void execute() {
+        commandElement.validate();
+        var url = formatUrl(commandElement);
+        String proccessCommand = commandToRun(url);
+        var pb = new ProcessBuilder(proccessCommand);
+        try {
+            pb.start();
+        } catch (Exception ex) {
+            throw CommandErrors.commandRunError(commandElement, ex);
+        }
+    }
+
+    private String formatUrl(CommandElement commandElement) {
+        var url = commandElement.getValue();
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            url = "https://" + url;
+        }
+        return url;
+    }
+
+    private String commandToRun(String url) {
+        var osType = OSType.getOSType();
+        return switch (osType) {
+            case Windows -> "cmd /c start " + url;
+            case Linux -> "xgd-open " + url;
+            default -> "open " + url;
+        };
+    }
 }
