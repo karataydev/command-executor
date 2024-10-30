c361ea3035e02aed70f61cac9b4218b53d978743
emre.karatay <emre.karatay@softtech.com.tr>
2024-10-30 17:00:47 +0300

    reader updated for '\' special chars in windows

--- command-executor/src/main/java/dev/karatay/commandexecutor/config/reader/FileReadStrategy.java
+++ command-executor/src/main/java/dev/karatay/commandexecutor/config/reader/FileReadStrategy.java
@@ -3,6 +3,7 @@ package dev.karatay.commandexecutor.config.reader;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Optional;
+import java.util.regex.Matcher;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,7 +27,7 @@ class FileReadStrategy implements ConfigReadStrategy {
 	@Override
 	public Optional<Config> read() {
 		try {
-			configPath = configPath.replaceFirst("^~", System.getProperty("user.home"));
+			configPath = configPath.replaceFirst("^~", Matcher.quoteReplacement(System.getProperty("user.home")));
 			var configMap = objectMapper.readValue(new File(configPath), new TypeReference<HashMap<String, CommandElement>>(){});
 			var config = new Config(configMap);
 			return Optional.of(config);
